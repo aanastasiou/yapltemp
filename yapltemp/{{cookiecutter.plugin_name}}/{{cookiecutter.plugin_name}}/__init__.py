eb647c4b7ec0ca7e2ed303f895eca9224c6839e9
Athanasios Anastasiou <athanastasiou@gmail.com>
2021-12-14 13:07:46 +0000

    Corrected the environment variable in __init__

--- yapltemp/{{cookiecutter.plugin_name}}/{{cookiecutter.plugin_name}}/__init__.py
+++ yapltemp/{{cookiecutter.plugin_name}}/{{cookiecutter.plugin_name}}/__init__.py
@@ -1,5 +1,5 @@
 # THIS FILE IS NOT PART OF {{cookiecutter.plugin_name}}
-# PLEASE DO NOT COPY IT ACROSS TO YOUR {YASARA_HOME_DIRECTORY>/plg WHEN INSTALLING THE PLUGIN
+# PLEASE DO NOT COPY IT ACROSS TO YOUR {YASARA_HOME>/plg WHEN INSTALLING THE PLUGIN
 #
 # This file makes the {{cookiecutter.plugin_name}}/ directory appear as a module and is only useful to sphinx when
 # generating the documentation
